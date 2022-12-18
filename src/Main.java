45ac911588cd827e3ad06d0b1276266fe02a83bf
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-12-18 00:44:10 -0500

    casting: By using casting we can put one number to another

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+
+
 public class Main {
 
-    static Byte mean = Byte.MAX_VALUE;
-    static Byte mean2 = Byte.MIN_VALUE;
-    public static void main(String[] args) {
-        System.out.println(mean);
-        System.out.print(mean2);
-    }
-}
+      static byte myMinByteValue = Byte.MIN_VALUE;
+    static byte myNewByteValue = (byte) (myMinByteValue / 2);
 
+    public static void main(String[] args) {
+
+        System.out.print(myNewByteValue );
+
+
+    }
+
+
+}

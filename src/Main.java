0b6833cbca6afd3cf148e9a73af5924f280544f1
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-12-18 00:50:47 -0500

    The primitive types

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 
 
 public class Main {
+         static     byte ch1 = 3;
+        static    short ch2 = 6;
+       static  int ch3 = -7;
+       static   long ch4 = 500 * (ch1 + ch2 + ch3);
 
-      static byte myMinByteValue = Byte.MIN_VALUE;
-    static byte myNewByteValue = (byte) (myMinByteValue / 2);
 
     public static void main(String[] args) {
 
-        System.out.print(myNewByteValue );
-
-
+        System.out.println(ch1 );
+        System.out.println(ch2);
+        System.out.println(ch3);
+        System.out.println(ch4);
     }
 
 

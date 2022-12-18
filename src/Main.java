ccaf4b1fea7056525f889f7cd9e0a831429adc04
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-12-18 11:32:31 -0500

    The casting with short(for converting one data type to another)

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 
 
 public class Main {
-         static     byte ch1 = 3;
-        static    short ch2 = 6;
-       static  int ch3 = -7;
-       static   long ch4 = 500 * (ch1 + ch2 + ch3);
+    static byte ch1 = 3;
+
+
+
+    static int ch3 = 2;
+
+    static short  shortTotal =    (short)   (1000 + 10 * (ch1 + ch3));
+
+
+    static long ch4 = 50000L + 10L * (ch1 +  ch3);
 
 
     public static void main(String[] args) {
 
-        System.out.println(ch1 );
-        System.out.println(ch2);
+        System.out.println(ch1);
         System.out.println(ch3);
         System.out.println(ch4);
+        System.out.println(shortTotal);
     }
 
 

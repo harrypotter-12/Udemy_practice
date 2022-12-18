19c8b97ae409ad408a855a927ce2f349a421d496
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-12-17 23:12:02 -0500

    Integer range method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
+    static int myMinIntValue = Integer.MIN_VALUE;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println( myMinIntValue);
+
     }
 }
+

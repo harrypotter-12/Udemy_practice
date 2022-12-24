79ac65f620f4bdd2fbbc63950462a28449ed5186
Jashanpreet1234 <Kaur0776@algonquinlive.com>
2022-12-24 12:22:13 -0500

    This is the if-else statements and the dollar sign thingy

--- src/IntelliJ.java
+++
@@ -1,8 +0,0 @@
-public class IntelliJ {
- static int number = 5;
-    public static void main(String[]args){
-        System.out.println("My favourite number\t" + number);
-        System.out.println("Hello,Tim");
-        System.out.print("Hello, World");
-    }
-}
